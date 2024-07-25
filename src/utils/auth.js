baa7fa062254f72515e4a840864f283f01240095
Web-CODED <progeng94@gmail.com>
2024-07-26 01:56:09 +0530

    Add files via upload

---
+++ src/utils/auth.js
@@ -0,0 +1,17 @@
+import GoogleProvider from "next-auth/providers/google"
+import FacebookProvider from "next-auth/providers/facebook"
+
+export const authOption = {
+        providers: [
+          GoogleProvider({
+              clientId: process.env.GOOGLE_ID,
+              clientSecret: process.env.GOOGLE_SECRET,
+          }),
+          FacebookProvider({
+              clientId: process.env.FACEBOOK_ID,
+              clientSecret: process.env.FACEBOOK_SECRET,
+          }),
+      
+        ],
+    
+};
